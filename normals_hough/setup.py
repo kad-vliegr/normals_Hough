c7594056ebb8417f358b8fb43ee75f008beefe5f
ShinNoNoir <shinnonoir+git@gmail.com>
2020-11-25 11:09:52 +0100

    Add numpy include and fix MSVC flags

--- normals_hough/setup.py
+++ normals_hough/setup.py
@@ -5,6 +5,15 @@ from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import numpy
+import platform
+
+if platform.system() == 'Windows':
+  COMPILE_ARGS = ["/openmp", "/std:c11"]
+else:
+  COMPILE_ARGS = ["-fopenmp", "-std=c++11"]
+
+
 setup(
     name = "Hough Normal Estimator",
     ext_modules = [
@@ -14,9 +23,9 @@ setup(
           "src/NormalEstimatorHough.pyx",
           "src/normEstHough.cxx"
         ],
-        include_dirs=["third_party_includes/"],
+        include_dirs=["third_party_includes/", numpy.get_include()],
         language="c++",
-        extra_compile_args = ["-fopenmp", "-std=c++11"],
+        extra_compile_args = COMPILE_ARGS,
         extra_link_args=['-lgomp']
     )],
     cmdclass = {'build_ext': build_ext},
